99ec5e2d01160d3149e8f45de1a4319bc058f130
lohith1601 <lohithganapathy16@gmail.com>
2024-06-11 20:48:44 +0530

    project links

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -6,20 +6,20 @@ import Image from 'next/image';
 
 const RecentProject = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id='projects'>
     <h1 className="heading">
       A small selection of{" "}
       <span className="text-purple">recent projects</span>
     </h1>
     <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-      {projects.map((item) => (
-        <div
+    {projects.map(({id,title,des,img,iconLists,link}) =>(
+          <div key={id}
           className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-          key={item.id}
+        
         >
           <PinContainer
-            title="/ui.aceternity.com"
-            href="https://twitter.com/mannupaaji"
+            title={link} 
+            href={link}
           >
             <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[33vh] mb-10">
               <div
@@ -29,14 +29,13 @@ const RecentProject = () => {
                   <Image src="/bg.png" alt="background image" layout="fill" objectFit="cover" />
                   </div>
               <img
-                src={item.img}
-                alt="cover"
+                src={img} alt={title}
                 className="z-10 absolute bottom-0"
               />
             </div>
 
             <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-              {item.title}
+            {title}
             </h1>
 
             <p
@@ -46,12 +45,12 @@ const RecentProject = () => {
                 margin: "1vh 0",
               }}
             >
-              {item.des}
+              {des}
             </p>
 
             <div className="flex items-center justify-between mt-7 mb-3">
               <div className="flex items-center">
-                {item.iconLists.map((icon, index) => (
+              {iconLists.map((icon,index)=>(
                   <div
                     key={index}
                     className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
